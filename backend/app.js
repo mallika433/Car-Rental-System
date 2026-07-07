e819d599c2a6660f255b9ceb06a69b5f370db3cf
“mallika433” <np03cs4s250046@heraldcollege.edu.np>
2026-07-07 12:06:48 +0545

    feat: refactor project into MVC architecture

--- backend/app.js
+++ backend/app.js
@@ -1,19 +1,15 @@
 import express from "express"
-import cars from "./data/cars.js"
+
+import carRoutes from './src/routes/carRoutes.js'
+import bookingRoutes from "./src/routes/bookingRoutes.js";
+
 
 const app = express()
 app.use(express.json())
 const PORT = 3001
 
-app.get("/cars", (req, res) => {
-    return res.json(cars)
-})
-
-app.post("/cars", (req, res) => {
-    const car = req.body
-    cars.push(car)
-    return res.status(201).json({"message": "Car added successfully"})
-})
+app.use(carRoutes);
+app.use(bookingRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
