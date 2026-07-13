31811c5897baae6eb52d5650fd95fedb1548ee92
“mallika433” <np03cs4s250046@heraldcollege.edu.np>
2026-07-13 12:05:56 +0545

    Added MongoDB connection

---
+++ backend/src/config/db.js
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+
+dotenv.config({
+    path:'../../.env'
+})
+
+const dbConnection = async()=> {
+    try{
+        await mongoose.connect(process.env.MONGODB_URI)
+        console.log("MongoDB successfully connected")
+    }catch(error){
+        console.log(`MongoDB cannot be connected ${error.message}`)
+        process.exit()
+    }
+}
+
+export default dbConnection
+
